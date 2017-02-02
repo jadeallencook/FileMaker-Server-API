e73b08e046aca55e1810c594a8e302dca862c405
Jade Allen Cook <jadeallencook@gmail.com>
2017-02-02 14:18:56 -0500

    ⛏ Added API and readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # FileMaker-Server-API
+
 API made using PHP to work with FileMaker databases using AJAX calls.
+
+```js
+$.ajax({
+    url: 'api.php',
+    method: 'post',
+    data: {
+        layout: 'settings'
+    },
+    success: function (settings) {
+        console.log(settings);
+    }
+});
+```
---
+++ api.php
@@ -0,0 +1,49 @@
+<?php
+
+    // get layout from ajax
+    $layoutName = $_POST['layout'];
+
+    // require fm libs
+    require_once('../fmview.php');
+    require_once('../FileMaker.php');
+    require_once('../error.php');
+
+    // cache database auth
+    $database = '';
+    $username = '';
+    $password = '';
+
+    // create new fm connection
+    $fm = & new FileMaker();
+    $fm->setProperty('database', $database);
+    $fm->setProperty('username', $username);
+    $fm->setProperty('password', $password);
+    
+    // cache fields 
+    $layout = $fm->getLayout($layoutName);
+    $fields = $layout->listFields();
+    $record = $fm->getRecordById($layoutName, 1);
+
+    // formats for dates and times
+    $displayDateFormat = '%m/%d/%Y';
+    $displayTimeFormat = '%I:%M %P';
+    $displayDateTimeFormat = '%m/%d/%Y %I:%M %P';
+    $submitDateOrder = 'mdy';
+
+    // create new json {}
+    $json = new stdClass();
+
+    // iterate over all fields
+    for ($i = 0; $i < count($fields); ++$i) {
+        // set nonset fields for null
+        if (empty($record->getField($fields[$i], 0))) {
+            $json->$fields[$i] = null;
+        } else {
+            $json->$fields[$i] = $record->getField($fields[$i], 0);
+        }
+    }
+
+    // return json {}
+    echo json_encode($json);
+
+?>
